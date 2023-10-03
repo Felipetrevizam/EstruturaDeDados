d5abd385f629b7be4218a7359ed2bbfbd2384b9d
Felipetrevizam <felipe_trevizam@icloud.com>
2023-10-02 21:55:59 -0300

    Adicionando Comentarios

--- EstruturaDinamica.c
+++ EstruturaDinamica.c
@@ -2,8 +2,8 @@
 
 int main(){
     int x = 25;
-    int* y = &x;
-    *y = 30;
+    int* y = &x; // y recebe o endereco de memoria de x
+    *y = 30; // 30 = endereco de memoria para onde y aponta que eh x
     printf("Valor atual de x: %i\n", x);
     return 0;
 }
